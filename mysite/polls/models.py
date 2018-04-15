63e53f8b632f4fba9dc88041719a91aabff0123e
Jay Harley Gadingan <jayharleygadz@gmail.com>
2018-04-16 02:05:02 +0800

    chapter7/TabularinLine

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta as tdelta
 
 # Create your models here.
 class Question(models.Model):
@@ -9,9 +11,9 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
-
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
